da0cd0e6e3d500abb93cc309f0f7f494b349d913
Tony Zhang <61944932+Tony1324@users.noreply.github.com>
2022-07-16 19:53:37 -0400

    highlighting, links, button changes

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,13 @@ export default function Home() {
 		
 		<main>
 			<h1>Mason Choi</h1>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<p> <a href="https://example.com">link</a> Lorem  ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 			<div className={styles.media}>
-				<button></button><button></button><button></button>
+				<button><p>github</p></button>
+				<button><p>email</p></button>
+				<button><p>stuff</p></button>
 			</div>
+			<h2>Projects</h2>
 		</main>
 		
 		<footer>
