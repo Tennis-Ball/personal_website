2f0a16786b244ad30add507c9c3e03183706d162
Tony Zhang <61944932+Tony1324@users.noreply.github.com>
2022-07-26 09:58:20 -0400

    theme toggle

--- components/nav.js
+++ components/nav.js
@@ -3,18 +3,49 @@ import styles from './nav.module.css'
 import { useEffect, useState } from 'react'
 
 export default function Nav(props){
+    const [theme, setTheme] = useState('')
+    const [browserTheme, setBrowserTheme] = useState('light')
+
+    function setHTMLAttrib(value,t){
+        if(value == 'auto'){
+            document.documentElement.setAttribute("theme", t ?? browserTheme)
+        }else{
+            document.documentElement.setAttribute("theme", value)
+        }   
+    }
+    useEffect(()=>{
+        const t = localStorage.getItem('theme')
+        setTheme(t ? t : 'auto')
+        setHTMLAttrib(t ? t : 'auto')
+        let listener = window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change",  (e) => {
+            setBrowserTheme(e.matches ? 'dark':'light')
+            setHTMLAttrib(theme, e.matches ? 'dark':'light')
+        })
+        return window.removeEventListener('change',listener)
+    })
     return (
         <div className={styles.container}>
             <A url="/">Home</A>
             <A url="/timeline">Timeline</A>
+            <div className="spacer"></div>
+            <select value={theme} onChange={(e)=>{
+                setTheme(e.target.value)
+                localStorage.setItem('theme',e.target.value)
+                setHTMLAttrib(e.target.value)
+            }} style={{display: `${theme ? "" : "none"}`}}>
+                <option value="auto">Auto</option>
+                <option value="light">Light</option>
+                <option value="dark">Dark</option>
+            </select>
         </div>
     )
+
 }
 
 
 function A(props){
 
-    let [isCurrent,setIsCurrent] = useState(false)
+    const [isCurrent,setIsCurrent] = useState(false)
 
     useEffect(()=>{
         setIsCurrent(window.location.pathname == props.url)
