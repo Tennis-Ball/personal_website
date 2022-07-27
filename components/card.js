b9d0a20802332b436b4a1d03297c2ab6fb3acaa6
Tony Zhang <61944932+Tony1324@users.noreply.github.com>
2022-07-27 12:40:05 -0400

    click anywhere on section to expand

--- components/card.js
+++ components/card.js
@@ -1,6 +1,6 @@
 import styles from './card.module.css'
 export default function Card(props){
     return (
-        <div className={`neutral ${styles.card}`}>{props.children}</div>
+        <div className={`neutral ${styles.card}`} onClick={(e)=>{e.stopPropagation()}}>{props.children}</div>
     )
 }
--- components/dropdown.js
+++ components/dropdown.js
@@ -13,7 +13,7 @@ export default function Dropdown(props){
         return ()=>{window.removeEventListener('resize',listener)}
     })
     return (
-        <div className={`${isOpen ? `accent ${styles.expanded}` : 'neutral'} ${styles.container}`}>
+        <div className={`${isOpen ? `accent ${styles.expanded}` : 'neutral'} ${styles.container}`} onClick={(e)=>{setIsOpen(!isOpen)}}>
             <div className={styles.bar}>
                 <h2>{props.title}</h2>
                 {!props.forceOpen &&
